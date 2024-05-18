d9eda8acbcc99ced113f949d7d8cadaf18901d1b
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-05-19 01:07:49 +0200

    fix(backend): security problems detected temp fix

--- src/main/java/it/savoreco/savoreco/filter/LoginFilter.java
+++ src/main/java/it/savoreco/savoreco/filter/LoginFilter.java
@@ -3,7 +3,6 @@ package it.savoreco.savoreco.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 
-import java.io.IOException;
 
 @WebFilter("/user")
 public class LoginFilter implements Filter {
