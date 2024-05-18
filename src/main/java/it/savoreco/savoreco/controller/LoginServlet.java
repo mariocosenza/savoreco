fe72a5f79387e300e292291361086b90b9b866f3
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-05-19 01:01:01 +0200

    feat(backend): add LoginServlet.java as a login page controller

---
+++ src/main/java/it/savoreco/savoreco/controller/LoginServlet.java
@@ -0,0 +1,17 @@
+package it.savoreco.savoreco.controller;
+
+import jakarta.servlet.annotation.HttpConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(
+        name = "loginPage",
+        value = "/login"
+)
+@ServletSecurity(
+       @HttpConstraint(transportGuarantee = ServletSecurity.TransportGuarantee.CONFIDENTIAL)
+)
+public class LoginServlet extends HttpServlet {
+
+}
