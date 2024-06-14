81572859ee05d75a97f3a8594664435bf01f605e
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-14 20:45:49 +0200

    feat(back-end): create RegistrationServlet file

---
+++ src/main/java/it/savoreco/controller/RegistrationServlet.java
@@ -0,0 +1,11 @@
+package it.savoreco.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(
+        name = "registrationPage",
+        value = "/registration"
+)
+public class RegistrationServlet {
+
+}
