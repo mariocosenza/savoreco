90b6dbfc9d1fd6015a520c44d2b65a3b3965cb66
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-19 18:25:56 +0200

    feat(back-end): add invalidate controller

---
+++ src/main/java/it/savoreco/controller/InvalidateServlet.java
@@ -0,0 +1,27 @@
+package it.savoreco.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+@WebServlet(value = "/exit")
+public class InvalidateServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(InvalidateServlet.class);
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)  {
+        if(req.getSession(false) != null) {
+            req.getSession().invalidate();
+            try {
+                resp.sendRedirect("/home");
+            } catch (IOException e) {
+                logger.warn("Error sending redirect to invalidate session", e);
+            }
+        }
+    }
+}
