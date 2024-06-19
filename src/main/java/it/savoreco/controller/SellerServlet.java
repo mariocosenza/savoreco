410bf85956704f102abde360b86ffe06374928d4
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-19 17:42:54 +0200

    style(refactor): refactor file

--- src/main/java/it/savoreco/controller/SellerServlet.java
+++ src/main/java/it/savoreco/controller/SellerServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 @WebServlet(
