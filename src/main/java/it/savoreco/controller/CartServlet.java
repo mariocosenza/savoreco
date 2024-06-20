4d183d186f0b2ddeb451cbaf0f3806693dc67e4d
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-20 18:21:49 +0200

    feat(backed-end): initial creation of CartServlet.java and PaymentServlet.java

--- src/main/java/it/savoreco/controller/CartServlet.java
+++ src/main/java/it/savoreco/controller/CartServlet.java
@@ -1,35 +1,100 @@
 package it.savoreco.controller;
+
+import it.savoreco.model.entity.*;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 
 @WebServlet(
         name = "cartServlet",
         displayName = "Savoreco - Carrello",
         description = "Carrello home page",
-        value = "/user/cart"
+        value = "/cart"
 )
 public class CartServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(CartServlet.class);
+
+    @SuppressWarnings("unchecked")
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/view/user/cart.jsp");
-        try {
-            requestDispatcher.forward(req, resp);
-        } catch (IOException | ServletException e) {
-            logger.warn("Cannot forward to cart.jsp", e);
+        SessionFactory sessionFactory = (SessionFactory) req.getServletContext().getAttribute("SessionFactory");
+        Session session = sessionFactory.getCurrentSession();
+        if (Objects.isNull(req.getParameter("add"))) {
+            RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/view/cart.jsp");
+            try {
+                if(Objects.isNull(req.getSession(false)) || Objects.isNull(req.getSession(false).getAttribute("cart"))) {
+                    req.setAttribute("noItem", true);
+                } else {
+                    if(Objects.nonNull(req.getSession(false).getAttribute("user"))) {
+                        var user = (UserAccount) req.getSession().getAttribute("user");
+                        Query<BasketContain> basketContainQuery = session.createQuery("from BasketContain b where b.basket.user = :user", BasketContain.class);
+                        basketContainQuery.setParameter("user", user);
+                        List<BasketContain> basketContains = basketContainQuery.list();
+                        req.setAttribute("basketList", basketContains);
+                    }
+                }
+                requestDispatcher.forward(req, resp);
+            } catch (IOException | ServletException e) {
+                logger.warn("Cannot forward to cart.jsp", e);
+            }
+        } else if (Objects.isNull(req.getSession(false)) || Objects.isNull(req.getSession(false).getAttribute("logged"))) {
+            var httpSession = req.getSession();
+            if (Objects.isNull(httpSession.getAttribute("cart"))) {
+                var cartMap = new HashMap<Integer, Integer>();
+                httpSession.setAttribute("cart", cartMap);
+            }
+            if (httpSession.getAttribute("cart") instanceof HashMap<?, ?> map) {
+                var typeMap = (HashMap<Integer, Integer>) map;
+                var quant = typeMap.get(Integer.parseInt(req.getParameter("foodId")));
+                if (quant != null) {
+                    quant++;
+                    typeMap.replace(Integer.parseInt(req.getParameter("foodId")), quant);
+                }
+            }
+        } else if (Objects.nonNull(req.getSession(false))
+                && Objects.nonNull(req.getSession().getAttribute("logged"))
+                && req.getSession().getAttribute("logged").equals("user")) {
+            var user = (UserAccount) req.getSession().getAttribute("user");
+            try {
+                Transaction transaction = session.beginTransaction();
+                Query<Basket> query = session.createQuery("FROM Basket b WHERE b.user = :user", Basket.class);
+                var quantity = 1;
+                var basket = query.stream().findAny().orElse(null);
+                if (basket == null) {
+                    basket = new Basket();
+                    basket.setUser(user);
+                    session.persist(basket);
+                }
+                var contains = new BasketContain();
+                var id = new BasketContainId();
+                Query<Food> foodQuery = session.createQuery("FROM Food WHERE id = :id", Food.class);
+                foodQuery.setParameter("id", Integer.parseInt(req.getParameter("id")));
+                var food = foodQuery.stream().findAny().orElseThrow();
+                id.setBasketId(basket.getId());
+                id.setFoodId(food.getId());
+                contains.setId(id);
+                contains.setBasket(basket);
+                contains.setQuantity(quantity);
+                contains.setFood(food);
+                session.persist(contains);
+                transaction.commit();
+            } catch (Exception e) {
+                logger.warn("Cannot persist cart.", e);
+            }
         }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
     }
 }
---
+++ src/main/java/it/savoreco/controller/PaymentServlet.java
@@ -0,0 +1,22 @@
+package it.savoreco.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(
+        name = "paymentServlet",
+        displayName = "Savoreco - Acquista",
+        description = "Acquista",
+        value = "/user/payment"
+)
+public class PaymentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
