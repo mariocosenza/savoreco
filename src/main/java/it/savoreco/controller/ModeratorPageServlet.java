5fe79aa43825626bec02cb9f7bc6e621139925ce
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-25 16:04:17 +0200

    refactor(back-end): remove unused import

--- src/main/java/it/savoreco/controller/ModeratorPageServlet.java
+++ src/main/java/it/savoreco/controller/ModeratorPageServlet.java
@@ -14,7 +14,6 @@ import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 
 @WebServlet(
