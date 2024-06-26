2b7116321f8277ae10f295727788475a88dfde94
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-26 23:00:45 +0200

    fix(back-end): fix ecopoint

--- src/main/java/it/savoreco/service/PointLevel.java
+++ src/main/java/it/savoreco/service/PointLevel.java
@@ -3,7 +3,7 @@ package it.savoreco.service;
 public class PointLevel {
     public static int calcolaLivello(int punti) {
         if (punti > 10) {
-            return (int) (Math.log10(punti));
+            return (int) (Math.log10(punti)) + 1;
         } else if (punti > 0) {
             return 1;
         } else {
@@ -13,21 +13,17 @@ public class PointLevel {
 
     public static int minLivello(int livello) {
         if (livello > 1) {
-            return (int) Math.pow(10, livello - 1) + 1;
-        } else if (livello == 1) {
-            return 1;
+            return (int) Math.pow(10, livello - 1);
         } else {
-            return 0;
+            return 0; // Livello 1 inizia da 0 punti
         }
     }
 
     public static int maxLivello(int livello) {
         if (livello > 1) {
             return (int) Math.pow(10, livello) - 1;
-        } else if (livello == 1) {
-            return 10;
         } else {
-            return 0;
+            return 10;
         }
     }
 }
