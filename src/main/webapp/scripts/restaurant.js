f4aef37057690875242beeb2936c90cf7060f6ca
MarioFas <mariofa03@gmail.com>
2024-06-26 19:39:50 +0200

    AddCart implementation

--- src/main/webapp/scripts/restaurant.js
+++ src/main/webapp/scripts/restaurant.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.add-to-cart');
 
     buttons.forEach(button => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', async function () {
             const foodId = this.getAttribute('data-food-id');
             const quantityElement = this.closest('.foodBox').querySelector('.foodInfo p strong + span');
 
@@ -25,32 +25,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 return;
             }
 
-            startEffect(this);
-
-            fetch('/restaurant', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ foodId: foodId })
-            })
-                .then(response => {
-                    if (response.ok) {
-                        console.log('Aggiunto al carrello');
-                        let quantity = parseInt(quantityElement.textContent);
-                        quantity -= 1;
-                        quantityElement.textContent = quantity;
-                        if (quantity <= 0) {
-                            this.disabled = true;
-                        }
-                    } else {
-                        console.error('Errore nell\'aggiungere al carrello');
-                    }
-                })
-                .catch(error => {
-                    console.error('Errore di rete:', error);
-                    window.location.href = "/home";
+            try {
+                const response = await fetch("/user/cart", {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                    },
+                    body: new URLSearchParams({
+                        foodId: foodId,
+                        add: "true"
+                    }),
                 });
+                if (response.ok) {
+                    startEffect(this);
+                    console.log('Aggiunto al carrello');
+                } else {
+                    console.error('Errore nell\'aggiungere al carrello');
+                }
+            } catch (e) {
+                console.error('Errore di rete:', error);
+                window.location.href = "/home";
+            }
         });
     });
 });
