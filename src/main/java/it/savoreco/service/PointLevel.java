465c955daf4810c7cbc20c6d7fd3291be9d703ea
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-26 20:16:23 +0200

    feat(back-end): add min and max level

--- src/main/java/it/savoreco/service/PointLevel.java
+++ src/main/java/it/savoreco/service/PointLevel.java
@@ -1,13 +1,33 @@
 package it.savoreco.service;
 
 public class PointLevel {
-        public static int calcolaLivello(int punti) {
-            if(punti > 10) {
-                return (int)(Math.log10(punti));
-            } else if(punti > 0) {
-                return 1;
-            } else {
-                return 0;
-            }
+    public static int calcolaLivello(int punti) {
+        if (punti > 10) {
+            return (int) (Math.log10(punti));
+        } else if (punti > 0) {
+            return 1;
+        } else {
+            return 0;
         }
+    }
+
+    public static int minLivello(int livello) {
+        if (livello > 1) {
+            return (int) Math.pow(10, livello - 1) + 1;
+        } else if (livello == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int maxLivello(int livello) {
+        if (livello > 1) {
+            return (int) Math.pow(10, livello) - 1;
+        } else if (livello == 1) {
+            return 10;
+        } else {
+            return 0;
+        }
+    }
 }
