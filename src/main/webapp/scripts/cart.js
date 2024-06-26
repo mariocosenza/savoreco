c23118f8fdb2c6ddaed797c71c5cf0fcc0f37afa
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-26 15:08:21 +0200

    fix(front-end): full user cart implementation

--- src/main/webapp/scripts/cart.js
+++ src/main/webapp/scripts/cart.js
@@ -1,4 +1,4 @@
-async function deleteProduct(obj, id) {
+async function deleteProduct(obj, id, price) {
     try {
         const response = await fetch("/user/cart", {
             method: "POST",
@@ -11,20 +11,48 @@ async function deleteProduct(obj, id) {
             }),
         });
         if (response.ok) {
-            console.log("ok")
+            const sel =  document.querySelector("#price")
+            const selPrice = parseFloat(sel.innerHTML)
+            if(selPrice - parseFloat(price) <= 0) {
+                location.reload();
+            } else {
+                sel.innerHTML = `${selPrice - parseFloat(price)}`
+            }
+            obj.closest("li").remove()
         } else {
-
+            location.reload();
         }
     } catch (e) {
         window.location.href = "/home"
     }
 }
 
-function addProduct (obj, id) {
-
+async function addProduct (obj, id, price) {
+    try {
+        const response = await fetch("/user/cart", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: new URLSearchParams({
+                foodId: id,
+                add: "true"
+            }),
+        });
+        if (response.ok) {
+            let num = parseInt(obj.parentElement.children[0].innerHTML)
+            num++
+            obj.parentElement.children[0].innerHTML = `${num}`
+            const sel =  document.querySelector("#price")
+            const selPrice = parseFloat(sel.innerHTML)
+            sel.innerHTML = `${selPrice + parseFloat(price)}`
+        } else {
+            location.reload();
+        }
+    } catch (e) {
+        console.log(e)
+        window.location.href = "/home"
+    }
 }
 
 
-function lessProduct (obj, id) {
-
-}
