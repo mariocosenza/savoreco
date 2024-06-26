f9865a7450131fb4724cbadc85074388e632d8d5
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-26 10:20:15 +0200

    fix(front-end): unused parameter

--- src/main/webapp/scripts/purchase.js
+++ src/main/webapp/scripts/purchase.js
@@ -123,7 +123,7 @@ function onGooglePaymentButtonClicked() {
     paymentsClient.loadPaymentData(paymentDataRequest)
         .then(function(paymentData) {
             // handle the response
-            processPayment(paymentData);
+            processPayment();
         })
         .catch(function(err) {
             // show error in developer console for debugging
