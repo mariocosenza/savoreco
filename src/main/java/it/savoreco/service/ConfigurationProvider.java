1d60eea55487272eccec648ae34480923a94e55f
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2024-06-26 21:34:40 +0200

    feat(back-end): add server config

--- src/main/java/it/savoreco/service/ConfigurationProvider.java
+++ src/main/java/it/savoreco/service/ConfigurationProvider.java
@@ -1,5 +1,7 @@
 package it.savoreco.service;
 
+import org.apache.commons.lang3.SystemUtils;
+
 import java.io.*;
 import java.util.NoSuchElementException;
 import java.util.Properties;
@@ -10,12 +12,15 @@ public class ConfigurationProvider {
     public ConfigurationProvider() {
         File file = new File("C:\\Users\\proge\\Desktop\\bucket.cfg.xml");
         InputStream inputStream;
-        try {
-            inputStream = new FileInputStream(file);
-        } catch (FileNotFoundException ignore) {
-            throw new NoSuchElementException("File not found");
+        if (SystemUtils.IS_OS_UNIX) {
+            inputStream = ConfigurationProvider.class.getClassLoader().getResourceAsStream("/bucket.cfg.xml");
+        } else {
+            try {
+                inputStream = new FileInputStream(file);
+            } catch (FileNotFoundException ignore) {
+                throw new NoSuchElementException("File not found");
+            }
         }
-
         readProperties(inputStream);
     }
 
